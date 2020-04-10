fdfe855c722e8776746b93fba9f0bd92b142eb07
Vendy Prum <vendy@thinkful.com>
2020-04-09 18:30:41 -0700

    server testing

---
+++ thingful-server/src/token-service.js
@@ -0,0 +1,12 @@
+import config from './config';
+const TokenService = {
+  makeBasicAuthToken(userName, password) {
+    return window.btoa(`${userName}:${password}`)
+  },
+  saveAuthToken(token) {
+    window.localStorage.setItem(config.TOKEN_KEY, token)
+  },
+};
+module.exports = {
+  TokenService
+};
